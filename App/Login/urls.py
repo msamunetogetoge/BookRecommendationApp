60b5b45ee2a98d4a1221d5072832cb56a7278ead
masamune <kenjiii534@gmail.com>
2021-11-26 08:56:55 +0900

    パスワードをお忘れの場合にリンクはる Fixes #26

--- App/Login/urls.py
+++ App/Login/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('config', views.user_config, name='config'),
     path('register', views.register_attr, name='register_attr'),
     path('delete', views.delete_attr, name='delete_attr'),
+    path('change_password', views.change_password, name='change_password'),
+    path('publish_code', views.check_and_publish_code, name="check_and_publish_code")
+   
 ]
