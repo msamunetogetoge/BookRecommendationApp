a03b3d25c83fd8ff609c638bff3bc8a30f48a5f3
masamune <kenjiii534@gmail.com>
2021-12-07 22:27:38 +0900

    django-environで環境変数を管理するように変更

--- App/App/settings.py
+++ App/App/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ LOGIN_URL = '/login'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yzc3a7*f_t9(^=46c$tr(e!sj86&v_c=vsv8=z(v2kd0oot=pj'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,14 +92,7 @@ WSGI_APPLICATION = 'App.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sample',
-        'USER': 'book',
-        'PASSWORD': 'book',
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
+    'default': env.db()
 }
 
 
