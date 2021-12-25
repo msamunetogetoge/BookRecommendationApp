7e68cf9b7ab094eb13a37f45bbf684cd6124c98f
masamune <kenjiii534@gmail.com>
2021-12-25 11:18:59 +0900

    書籍検索した時、著者が出ない事があるので直す Fixes #55

--- App/utils/api_search.py
+++ App/utils/api_search.py
@@ -35,8 +35,7 @@ def search_from_params(auth:str="", title:str="")->list[dict]:
     if url == "":
         return list({})
     res = requests.get(url).json()
-    print(url)
-    result_list = get_result_list(res)
+    result_list = get_result_list(res, auth)
     return result_list
     
 
@@ -68,7 +67,7 @@ def create_query_string(title="", auth="")->str:
     url +=  "&maxResults=40"
     return url
 
-def get_result_list(response:dict)-> list[dict]:
+def get_result_list(response:dict, author:str="")-> list[dict]:
     if response['totalItems'] == 0:
         return list({})
     result_dict =[]
@@ -85,6 +84,10 @@ def get_result_list(response:dict)-> list[dict]:
                 dic[k] = {'thumbnail':"/static/noimage.png"}
             else:
                 dic[k] = ""
+        # author 指定がある時は、authorが空の場所にauthorを挿入する
+        if author != "" and ( dic[GoogleBooksStrs.authors] in [[], None, ""]):
+            dic[GoogleBooksStrs.authors] =[author]
+
         result_dict.append(dic)
     return result_dict
 
