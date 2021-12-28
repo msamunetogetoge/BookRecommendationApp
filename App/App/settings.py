8d77c9f72626650d3e55bc5ddc76b7606918abd3
masamune <kenjiii534@gmail.com>
2021-12-29 00:11:19 +0900

    debug か否かも.envで管理するように変更

--- App/App/settings.py
+++ App/App/settings.py
@@ -32,7 +32,7 @@ LOGIN_URL = '/login'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
